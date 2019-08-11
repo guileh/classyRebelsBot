0aa54c24c1ba7dfbed7b6199fec074e9836b62dd
Guilherme Rocha <guilherme@candiba.net>
2019-08-11 19:21:14 -0300

    Separei em modulos

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -8,5 +8,3 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 HTTP_URI = os.getenv("BASE_HTTP_URL")
 
-
-HTTP_PLAYER_NAME="https://search-api.tracker.network/d2players?q="
--- src/core.py
+++ src/core.py
@@ -2,9 +2,10 @@ import json
 import requests
 import traceback
 
+from search_player_name import *
 
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-from conf.settings import TELEGRAM_TOKEN, HTTP_URI, HTTP_PLAYER_NAME
+from conf.settings import TELEGRAM_TOKEN, HTTP_URI
 
 
 def start(bot, update):
@@ -21,34 +22,6 @@ def unknown(bot, update):
         text=response_message
     )
 
-def search_player_name(bot, update, args):
-    response_message = "Localizei esses guardioes: \n\n"
-    try:
-        response = requests.get(HTTP_PLAYER_NAME+args[0])
-        data = json.loads(response.content)
-
-        if len(data) > 0:
-            for value in data:
-                if value['platform'] == 1 :
-                    platform = "XBX"
-                elif value['platform'] == 2 :
-                    platform = "PS4"
-                elif value['platform'] == 4:
-                    platform = "_PC"
-                else:
-                    platform = "___"
-                response_message = response_message + "  - [" + platform + "] " + value['name'] + "\n"
-        else:
-            response_message = "Nao existem guardioes com esse padrao, procure por NinoKusanagi que eh um nome muito mais comum."
-
-    except Exception:
-        traceback.print_exc()
-
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message
-    )
-
 def main():
     updater = Updater(token=TELEGRAM_TOKEN)
 
@@ -59,7 +32,7 @@ def main():
     )
     #USER COMMAND INTERFACE
     dispatcher.add_handler(
-        CommandHandler('buscar', search_player_name, pass_args=True)
+        CommandHandler('buscargt', search_player_name, pass_args=True)
     )
     #IF COMMAND IS FALSE
     dispatcher.add_handler(
